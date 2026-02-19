f64104b5873a82694eadfa24f6fba11055e9b736
labstraction <labstraction@protonmail.com>
2026-02-19 11:48:20 +0100

    ngClass

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,38 @@
+
+import { NgClass } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgClass],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
+
+
   protected readonly title = signal('AngularDirectives');
+
+  firstNumber = 0;
+  secondNumber = 0;
+
+  result = 0;
+  isResultEven = true;
+
+  calculate() {
+    this.result = this.firstNumber + this.secondNumber;
+    if(this.result % 2 === 0){
+      this.isResultEven = true;
+    } else {
+      this.isResultEven = false;
+    }
+  }
+
+  look() {
+    console.log(this.secondNumber)
+  }
+  
 }
